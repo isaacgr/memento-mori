b02aacd70a1655a63d50eed7dd4c55a06c04c805
isaacgr <isaacgrowell@gmail.com>
2023-01-14 13:29:45 -0500

    Improve grid accuracy

--- app.js
+++ app.js
@@ -4,15 +4,53 @@ Date.prototype.addDays = function (days) {
   return date;
 };
 
+/**
+ * Get the total number of weeks between the date object and current date
+ *
+ * From:
+ *  https://stackoverflow.com/questions/6117814/get-week-of-year-in-javascript-like-in-php
+ *
+ * Algorithm is to find nearest thursday, it's year
+ * is the year of the week number. Then get weeks
+ * between that date and the first day of that year.
+ *
+ * Note that dates in one year can be weeks of previous
+ * or next year, overlap is up to 3 days.
+ *
+ * e.g. 2014/12/29 is Monday in week  1 of 2015
+ *      2012/1/1   is Sunday in week 52 of 2011
+ *
+ * @see https://en.wikipedia.org/wiki/ISO_week_date
+ * @see https://stackoverflow.com/questions/6117814/get-week-of-year-in-javascript-like-in-php
+ *
+ * @returns {number} Weeknumber for date object
+ */
 Date.prototype.getWeekNumber = function () {
-  var d = new Date(
-    Date.UTC(this.getFullYear(), this.getMonth(), this.getDate())
+  // Copy date so don't modify original
+  const d = new Date(
+    this.getUTCFullYear(),
+    this.getUTCMonth(),
+    this.getUTCDate()
   );
-  var dayNum = d.getUTCDay() || 7;
-  d.setUTCDate(d.getUTCDate() + 4 - dayNum);
+  // Set to nearest Thursday: current date + 4 - current day number
+  // Make Sunday's day number 7
+  d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
+  // Get first day of year
   var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+  // Calculate full weeks to nearest Thursday
   return Math.ceil(((d - yearStart) / 86400000 + 1) / 7);
 };
+
+/**
+ * Returns the number of weeks between two dates
+ *
+ * @param {Date} d1
+ * @param {Date} d2
+ * @returns {number} Number of weeks between d1 and d2
+ */
+function weeksBetween(d1, d2) {
+  return Math.round((d2 - d1) / (7 * 24 * 60 * 60 * 1000));
+}
 
 function getDates(startDate, stopDate) {
   var dateArray = new Array();
@@ -25,11 +63,8 @@ function getDates(startDate, stopDate) {
 }
 
 function isSameWeek(date1, date2) {
-  if (
-    date1.getFullYear() === date2.getFullYear() &&
-    date1.getMonth() === date2.getMonth() &&
-    date1.getWeekNumber() === date2.getWeekNumber()
-  ) {
+  const startDate = getStartDate();
+  if (weeksBetween(startDate, new Date(date1)) + 1 == date2){
     return true;
   }
   return false;
@@ -43,15 +78,6 @@ function getWeekNumber(year, week) {
   return year * 52 - (53 - week) + 1;
 }
 
-function getGridDate(week) {
-  const startDate = getStartDate();
-  const day = 1 + (week - 1) * 7;
-  return new Date(
-    startDate.getFullYear(),
-    startDate.getMonth(),
-    startDate.getDate() + day
-  );
-}
 
 function getDatesArray() {
   return getDates(getStartDate(), new Date());
@@ -67,9 +93,9 @@ function graduatedUni() {
   return new Date(startDate.getFullYear() + 24, 3, 15);
 }
 
-function weMet() {
+function metWife() {
   const startDate = getStartDate();
-  return new Date(startDate.getFullYear() + 24, 5, 16);
+  return new Date(startDate.getFullYear() + 24, 5, 7);
 }
 
 function covid() {
@@ -92,18 +118,23 @@ function graduatedHigh() {
   return new Date(startDate.getFullYear() + 17, 3, 16);
 }
 
+function firstPostSecJob() {
+  const startDate = getStartDate();
+  return new Date(startDate.getFullYear() + 24, 5, 16);
+}
+
 function getTdClassName(currYear, currWeek, numWeeks) {
-  const weekNumber = getWeekNumber(currYear, currWeek);
-  const gridDate = getGridDate(weekNumber);
-  if (isSameWeek(dadsDeath(), gridDate)) return "fill-black";
-  else if (isSameWeek(dadsDeathDate(), gridDate)) return "fill-black";
-  else if (isSameWeek(weMet(), gridDate)) return "fill-yellow";
-  else if (isSameWeek(graduatedCollege(), gridDate)) return "fill-red";
-  else if (isSameWeek(graduatedUni(), gridDate)) return "fill-blue";
-  else if (isSameWeek(getStartDate(), gridDate)) return "fill-white";
-  else if (isSameWeek(covid(), gridDate)) return "fill-green";
-  else if (isSameWeek(graduatedHigh(), gridDate)) return "fill-orange";
-  else if (getWeekNumber(currYear, currWeek) <= numWeeks) return "fill";
+  const gridWeek = getWeekNumber(currYear, currWeek);
+  if (isSameWeek(dadsDeath(), gridWeek)) return "fill-black";
+  else if (isSameWeek(dadsDeathDate(), gridWeek)) return "fill-black";
+  else if (isSameWeek(metWife(), gridWeek)) return "fill-pink";
+  else if (isSameWeek(graduatedCollege(), gridWeek)) return "fill-red";
+  else if (isSameWeek(graduatedUni(), gridWeek)) return "fill-blue";
+  else if (isSameWeek(getStartDate(), gridWeek)) return "fill-white";
+  else if (isSameWeek(covid(), gridWeek)) return "fill-green";
+  else if (isSameWeek(graduatedHigh(), gridWeek)) return "fill-orange";
+  else if (isSameWeek(firstPostSecJob(), gridWeek)) return "fill-yellow";
+  else if (gridWeek <= numWeeks) return "fill";
   else return "";
 }
 
